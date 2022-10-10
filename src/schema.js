1e91e821cf3a45f6a6b3ad703acf08edd82ccf14
Eloi <recycled_loops@hotmail.com>
2022-10-10 10:38:53 +0200

    refactor schema

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
@@ -35,8 +35,10 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
+    "This key returns duration in seconds"
+    durationInSeconds: Int
     "The track's approximate length to complete, in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use in minutes instead")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -62,7 +64,9 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use in minutes instead")
+    "This key returns duration in seconds"
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
